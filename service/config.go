71219fd94d4d1e00cc8b871fb60cdce4db7bed5f
Oliver Beattie <oliver@obeattie.com>
2015-07-02 12:10:47 +0100

    Correct comment

--- service/config.go
+++ service/config.go
@@ -7,6 +7,6 @@ import (
 type Config struct {
 	Name        string
 	Description string
-	// Transport specifies a transport to run the server on. If none is specified, a Rabbit transport is used.
+	// Transport specifies a transport to run the server on. If none is specified, a mock transport is used.
 	Transport transport.Transport
 }
