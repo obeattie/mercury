51cff199f2c1fdf0ccdacc711ba56ea9a33a0c31
Matt Heath <matt@mattheath.com>
2015-08-25 13:48:31 +0100

    Add extractors to pull service and endpoint from a context

--- requesttree/middleware.go
+++ requesttree/middleware.go
@@ -70,3 +70,35 @@ func (m requestTreeMiddleware) ProcessServerResponse(rsp mercury.Response, ctx c
 func Middleware() requestTreeMiddleware {
 	return requestTreeMiddleware{}
 }
+
+// OriginServiceFor returns the originating service for this context
+func OriginServiceFor(ctx context.Context) string {
+	if s, ok := ctx.Value(originServiceHeader).(string); ok {
+		return s
+	}
+	return ""
+}
+
+// OriginEndpointFor returns the originating endpoint for this context
+func OriginEndpointFor(ctx context.Context) string {
+	if e, ok := ctx.Value(originEndpointHeader).(string); ok {
+		return e
+	}
+	return ""
+}
+
+// CurrentServiceFor returns the current service that this context is executing within
+func CurrentServiceFor(ctx context.Context) string {
+	if s, ok := ctx.Value(currentServiceHeader).(string); ok {
+		return s
+	}
+	return ""
+}
+
+// CurrentEndpointFor returns the current endpoint that this context is executing within
+func CurrentEndpointFor(ctx context.Context) string {
+	if e, ok := ctx.Value(currentEndpointHeader).(string); ok {
+		return e
+	}
+	return ""
+}
